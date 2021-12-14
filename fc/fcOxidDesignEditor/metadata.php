785ee67f3feaefbd659750e3fd91ca8537e00d53
Mikolai Hecht <mikolai.hecht@fatchip.de>
2021-12-14 10:25:01 +0100

    0080285 changed metadata version

--- fc/fcOxidDesignEditor/metadata.php
+++ fc/fcOxidDesignEditor/metadata.php
@@ -14,7 +14,7 @@ $aModule = [
         'en' => 'OXID module design editor.',
     ),
     'thumbnail' => '',
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'author' => 'FATCHIP GmbH',
     'email' => 'support@fatchip.de',
     'extend' => [
