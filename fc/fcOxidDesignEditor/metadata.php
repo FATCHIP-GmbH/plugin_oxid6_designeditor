212c6db1042a1a6d219b587e4f00992be3a0ff42
FATCHIP GmbH <kontakt@fatchip.de>
2021-12-16 22:16:20 +0100

    Update Version number

--- fc/fcOxidDesignEditor/metadata.php
+++ fc/fcOxidDesignEditor/metadata.php
@@ -14,7 +14,7 @@ $aModule = [
         'en' => 'OXID module design editor.',
     ),
     'thumbnail' => '',
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'author' => 'FATCHIP GmbH',
     'email' => 'support@fatchip.de',
     'extend' => [
